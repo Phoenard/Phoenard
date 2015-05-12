b056657ff2309faa0e133f048db7065a02da4719
bergerkiller <bergerkiller@gmail.com>
2015-05-12 16:54:01 +0200

    Added test routine sketch
    Signed-off-by: bergerkiller <bergerkiller@gmail.com>

---
+++ examples/TestRoutine/TestRoutine.ino
@@ -0,0 +1,164 @@
+/*
+ * This is a full-fledged test routine used to validate Phoenard operation.
+ * All connections to on-board components are tested where possible.
+ * For sensors, an additional damage check is performed.
+ * This means sample data is measured and the range/deviation is checked.
+ * Heavily fluctuating or extreme outputs indicate hardware failure.
+ *
+ * For testing the connector, the Phoenard must be tested in our test station.
+ * This test station also performs the flashing of the bootloader/uploading.
+ * On startup a check is performed whether the test station is active.
+ *
+ * All tests are located in the tests.h file.
+ */
+#include "Phoenard.h"
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+#include <SFE_BMP180.h>
+#include "Tests.h"
+
+int testIndex = 0;
+
+TestResult doTest(char* what, TestResult(*testFunc)(void)) {
+  // Check if pressed - if pressed for longer than 1 second, show message
+  long sel_start = millis();
+  while(isSelectPressed() && (millis() - sel_start) < 1000);
+
+  // Wait until the SELECT key is no longer pressed
+  if (isSelectPressed()) {
+    showMessage("Please release the SELECT button\n"
+                "Indicates hardware problem if not pressed");
+
+    while (isSelectPressed());
+    showMessage("");
+  }
+  
+  Serial.print(what);
+  Serial.print(" Testing...");
+  
+  // Show testing state
+  showMessage("");
+  showStatus(testIndex, YELLOW, what, "Testing...");
+
+  // Do the test
+  TestResult result = testFunc();
+
+  // Show result
+  showStatus(testIndex, result.success ? GREEN : RED, what, result.status);
+  Serial.print(result.success ? " SUCCESS" : " FAILURE");
+  Serial.print(" - ");
+  Serial.println(result.status);
+
+  // Next test
+  testIndex++;
+  
+  return result;
+}
+
+void showStatus(int index, color_t color, char* what, char* text) {
+  const int w = 310;
+  const int h = 13;
+  int x = 5;
+  int y = 5 + index * (h + 5);
+  display.fillRect(x, y, w, h, color);
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(x + 3, y + 3);
+  display.print(what);
+  display.print(": ");
+  display.print(text);
+}
+
+void setup() {
+  Serial.begin(57600);
+
+  delay(20);
+
+  // Send out a token indicating we request a test station
+  Serial.println("Ready");
+
+  // Wait for a very short time for a response back to initiate test station mode
+  isStationConnected = readToken(Serial, "Station OK", 200);
+
+  // Setup eeprom to load home menu without saving on next reset
+  if (isStationConnected) {
+    PHN_Settings settings;
+    memcpy(settings.sketch_toload, "SKETCHES", 8);
+    settings.flags = SETTINGS_LOAD;
+    PHN_Settings_Save(settings);
+  }
+
+  // LCD Test
+  startLCDTest();
+
+  // Indicate testing has started
+  Serial.println("Testing started.");
+
+  // Turn on SIM module as needed
+  // Do this in two steps to save time
+  sim.init();
+  long sim_on_time = millis();
+  if (sim.isOn()) {
+    sim_on_time -= SIM_PWR_DELAY;
+  } else {
+    digitalWrite(SIM_PWRKEY_PIN, HIGH);
+  }
+
+  doTest("Connector", testConnector);
+  doTest("Touchscreen", testTouchscreen);
+  doTest("MIDI", testMIDI);
+  doTest("MP3", testMP3);
+  doTest("BMP180", testBMP180);
+  doTest("MPU6050", testMPU6050);
+  doTest("HMC5883L", testHMC5883L);
+  doTest("SRAM", testRAM);
+  doTest("Micro-SD", testSD);
+  doTest("WiFi", testWiFi);
+  doTest("Bluetooth", testBluetooth);
+
+  // Wait until delay has elapsed
+  while ((millis() - sim_on_time) < SIM_PWR_DELAY);
+  digitalWrite(SIM_PWRKEY_PIN, LOW);
+
+  // Perform testing
+  doTest("SIM908", testSIM);
+
+  sim.end();
+
+  // All done!
+  Serial.println("Testing completed.");
+}
+
+void loop() {
+}
+
+void startLCDTest() {
+  // Show the LCD Version in Serial for debugging purposes
+  Serial.print("LCD Version ID: ");
+  Serial.println(PHNDisplayHW::readRegister(0), HEX);
+  Serial.println("Press SELECT to continue...");
+
+  // As a first test, show RGB colors on the screen to verify readout works as expected
+  display.fillRect(0, 0, 107, 240, RED);
+  display.fillRect(107, 0, 106, 240, GREEN);
+  display.fillRect(213, 0, 107, 240, BLUE);
+  display.setCursor(30, 30);
+  display.setTextColor(WHITE);
+  display.setTextSize(3);
+  display.print("LCD Test Screen");
+  display.setTextSize(2);
+  display.setCursor(70, 100);
+  display.print("Press SELECT to");
+  display.setCursor(70, 120);
+  display.print("start the test");
+  
+  // Wait for SELECT pressed
+  while (!isSelectPressed());
+  
+  // Wipe screen
+  display.fill(BLACK);
+}
---
+++ examples/TestRoutine/Tests.h
@@ -0,0 +1,575 @@
+#include "Phoenard.h"
+
+/* All the BAUD rates to probe at */
+int baud_rates[] = {9600, 115200, 19200, 38400, 57600, 4800, 2400, 1200, 230400};
+const int baud_rates_cnt = sizeof(baud_rates) / sizeof(int);
+
+void calc_stats(int16_t *values, int cnt, int32_t *mean, int32_t *variation) {
+  int32_t diff;
+  *mean = 0;
+  for (int i = 0; i < cnt; i++) {
+    *mean += values[i];
+  }
+  *mean /= cnt;
+  *variation = 0;
+  for (int i = 0; i < cnt; i++) {
+    diff = values[i] - *mean;
+    *variation += (diff * diff);
+  }
+  *variation /= cnt;
+  *variation = (int32_t) sqrt(*variation);
+}
+
+void print_sensor_info(int32_t *mean, int32_t *deviation, int count) {
+  Serial.print("mean=[");
+  for (int i = 0; i < count; i++) {
+    if (i) Serial.print(", ");
+    Serial.print(mean[i]);
+  }
+  Serial.print("]  deviation=[");
+  for (int i = 0; i < count; i++) {
+    if (i) Serial.print(", ");
+    Serial.print(deviation[i]);
+  }
+  Serial.println("]");
+}
+
+typedef struct TestResult {
+  boolean success;
+  char status[50];
+  
+  TestResult(const TestResult& result) {
+    this->success = result.success;
+    memcpy(this->status, result.status, sizeof(status));
+  }
+  TestResult(boolean success, char* status) {
+    this->success = success;
+    memcpy(this->status, status, strlen(status) + 1);
+  }
+  TestResult(boolean success, String status) {
+    this->success = success;
+    status.toCharArray(this->status, sizeof(this->status));
+  }
+} TestResult;
+
+TestResult SUCCESS_RESULT(true, "No problems found");
+
+boolean isStationConnected = false;
+
+boolean readToken(Stream &serial, char *token, long timeoutMS) {
+  uint16_t i = 0;
+  long startTime = millis();
+  while (token[i]) {
+    if ((millis() - startTime) >= timeoutMS) {
+      // Timeout
+      return false;
+    }
+    if (serial.available() && serial.read() != token[i++]) i = 0;
+  }
+  return true;
+}
+
+boolean writeATCommand(Stream &serial, char *command, char* response, long timeoutMS) {
+  for (int i = 0; i < 3; i++) {
+    serial.print(command);
+    if (readToken(serial, response, timeoutMS)) return true;
+  }
+  return false;
+}
+
+#define DELIMITER  45
+
+bool SpiRAMTest(uint16_t address, char data_byte) {
+  sram.write(address, data_byte);
+  char result = sram.read(address);
+  if (result == data_byte) return true;
+
+  Serial.print("Data Error at address [");
+  Serial.print(address);
+  Serial.print("]: ");
+  Serial.print(result);
+  Serial.print(" != ");
+  Serial.print((uint8_t) data_byte);
+  Serial.print(" ");
+  return false;
+}
+
+// Shows a counter on the LCD screen
+void showCounter(int ctr) {
+  display.setTextColor(YELLOW, BLACK);
+  display.setCursor(320-20, 240-15);
+  display.print(ctr);
+}
+
+// Show a message on the LCD screen
+void showMessage(const char* message) {
+  const int msg_w = 320;
+  const int msg_h = 30;
+  const int msg_x = 0;
+  const int msg_y = 240 - msg_h;
+  display.fillRect(msg_x, msg_y, msg_w, msg_h, BLACK);
+  display.setTextColor(YELLOW);
+  display.drawStringMiddle(msg_x, msg_y, msg_w, msg_h, message);
+  
+  // Also print to SERIAL
+  if (*message) {
+    Serial.print("[");
+    while (*message) {
+      if (*message == '\n') {
+        Serial.print(' ');
+      } else {
+        Serial.print(*message);
+      }
+      message++;
+    }
+    Serial.print("]");
+  }
+}
+
+boolean isScreenTouched() {
+  uint16_t x, y, z1, z2;
+  PHNDisplayHW::readTouch(&x, &y, &z1, &z2);
+  return z1 != 0;
+}
+
+boolean isSelectPressed() {
+  return !digitalRead(SELECT_PIN);
+}
+
+/** ========================================================== **/
+
+TestResult testTouchscreen() {
+  // First check that the touchscreen is NOT touched right now
+  if (isScreenTouched()) {
+    showMessage("Please release the touchscreen\n"
+                "If not touching; indicates failure");
+
+    for (int i = 5; i >= 1; i--) {
+      showCounter(i);
+      long t = millis();
+      while (isScreenTouched() && (millis() - t) < 1000);
+    }
+    if (isScreenTouched()) {
+      return TestResult(false, "Touch is always detected");
+    }
+  }
+
+  // Wait until the screen is touched
+  showMessage("Please touch the touchscreen\n"
+              "Press in the MIDDLE of the screen");
+  for (int i = 5; i >= 1; i--) {
+    showCounter(i);
+
+    long t = millis();
+    while ((millis() - t) < 1000) {
+      uint16_t x, y, z1, z2;
+      PHNDisplayHW::readTouch(&x, &y, &z1, &z2);
+      if (z1 == 0) {
+        continue;
+      }
+
+      Serial.println();
+      Serial.print("  TOUCH: [");
+      Serial.print(x); Serial.print(", ");
+      Serial.print(y); Serial.print(", ");
+      Serial.print(z1); Serial.print(", ");
+      Serial.print(z2); Serial.println("]");
+
+      if (x <= 200 || x >= 800 || y <= 200 || y >= 800) {
+        return TestResult(false, "Read touch was out of bounds");
+      }
+
+      return SUCCESS_RESULT;
+    }
+  }
+
+  // Timeout - fail
+  return TestResult(false, "Touch is never detected");
+}
+
+TestResult testConnector() {
+  // Wait with timeout for a token response to be read back
+  if (!isStationConnected) {
+    return TestResult(true, "Skipped: requires test station");
+  }
+  
+  // In here is a polling response system for performing the tests
+  // No fancy protocol; just raw communication
+  char buff[200];
+  int buff_idx = 0;
+  while (true) {
+    // Wait for data
+    while (!Serial.available());
+    char c = Serial.read();
+    
+    // Process the message on every newline
+    if (c == '\n' || c == '\r') {
+      buff[buff_idx] = 0;
+      
+      boolean setting = !memcmp(buff, "SET", 3);
+      if (setting || !memcmp(buff, "GET", 3)) {
+        char mode = buff[3];
+        int pin = atoi(buff + 4);
+        if (setting) {
+          if (mode == 'L' || mode == 'H') {
+            pinMode(pin, OUTPUT);
+          } else {
+            pinMode(pin, INPUT);
+          }
+          if (mode == 'H' || mode == 'P') {
+            digitalWrite(pin, HIGH);
+          } else {
+            digitalWrite(pin, LOW);
+          }
+        } else {
+          if (digitalRead(pin)) {
+            Serial.print('H');
+          } else {
+            Serial.print('L');
+          }
+        }
+      } else if (!memcmp(buff, "FAIL", 4)) {
+        char message[220];
+        strcpy(message, "Failure of pin(s) ");
+        strcpy(message + strlen(message), buff + 4);
+        return TestResult(false, message);
+      } else if (!memcmp(buff, "PASS", 4)) {
+        return SUCCESS_RESULT;
+      }
+      buff_idx = 0;
+    } else {
+      buff[buff_idx++] = c;
+    }
+  }
+  
+}
+
+TestResult testBMP180() {
+  SFE_BMP180 barometer;
+  if (!barometer.begin()) {
+    return TestResult(false, "Failed to establish connection");
+  }
+  char status;
+  
+  // Perform several measurements of temperature and pressure
+  // Check if all goes well, then do a variance check as well
+  const int MEAS_CNT = 10;
+  int16_t temp_values[MEAS_CNT];
+  int16_t press_values[MEAS_CNT];
+  for (int i = 0; i < MEAS_CNT; i++) {
+    // Short delay
+    delay(100);
+    
+    // Measure and validate the temperature
+    double temperature;
+    status = barometer.startTemperature();
+    if (!status) return TestResult(false, "Failed to measure temperature");
+    delay(status);
+    barometer.getTemperature(temperature);
+    if (temperature < -40.0 || temperature >= 85.0) {
+      Serial.print("TEMP EXCEEDS: ");
+      Serial.print(temperature);
+      return TestResult(false, "Temperature exceeds sensor limits");
+    }
+    temp_values[i] = (int16_t) temperature;
+
+    // Measure and validate the pressure in millibar
+    double pressure;
+    status = barometer.startPressure(1);
+    if (!status) return TestResult(false, "Failed to measure pressure");
+    delay(status);
+    barometer.getPressure(pressure, temperature);
+    pressure *= 10.0; // Convert to Pascals
+    if (pressure <= 8000.0 || pressure >= 12000) {
+      Serial.print("PRESSURE EXCEEDS: ");
+      Serial.print(pressure);
+      Serial.print("Pa");
+      return TestResult(false, "Pressure exceeds sensor limits");
+    }
+    press_values[i] = (int16_t) pressure;
+  }
+  
+  // Calculate statistics
+  int32_t temp_mean, temp_dev, press_mean, press_dev;
+  calc_stats(temp_values, MEAS_CNT, &temp_mean, &temp_dev);
+  calc_stats(press_values, MEAS_CNT, &press_mean, &press_dev);
+  
+  // Print the measurement data
+  Serial.println();
+  Serial.print("  TEMPERATURE: ");
+  print_sensor_info(&temp_mean, &temp_dev, 1);
+  Serial.print("  PRESSURE: ");
+  print_sensor_info(&press_mean, &press_dev, 1);
+
+  // Check for out of bounds data
+  if (abs(temp_dev) > 3) return TestResult(false, "Temperature readout is unstable");
+  if (abs(press_dev) > 3) return TestResult(false, "Pressure readout is unstable");
+
+  // All good!
+  return SUCCESS_RESULT;
+}
+
+TestResult testMPU6050() {
+  MPU6050 accelgyro(MPU6050_ADDRESS_AD0_LOW);  
+  Wire.begin();
+  accelgyro.initialize();
+  if (!accelgyro.testConnection()) {
+    return TestResult(false, "Failed to establish connection");
+  }
+
+  // Generate 5 samples of accelerometer and gyro data
+  const int MEAS_CNT = 5;
+  int16_t accel_x[MEAS_CNT];
+  int16_t accel_y[MEAS_CNT];
+  int16_t accel_z[MEAS_CNT];
+  int16_t gyro_x[MEAS_CNT];
+  int16_t gyro_y[MEAS_CNT];
+  int16_t gyro_z[MEAS_CNT];
+  for (int i = 0; i < MEAS_CNT; i++) {
+    delay(100);
+    accelgyro.getAcceleration(&accel_x[i], &accel_y[i], &accel_z[i]);
+    accelgyro.getRotation(&gyro_x[i], &gyro_y[i], &gyro_z[i]);
+  }
+
+  // Generate mean and deviation information from the gathered data
+  int32_t accel_mean[3];
+  int32_t accel_dev[3];
+  int32_t gyro_mean[3];
+  int32_t gyro_dev[3];
+  calc_stats(accel_x, MEAS_CNT, &accel_mean[0], &accel_dev[0]);
+  calc_stats(accel_y, MEAS_CNT, &accel_mean[1], &accel_dev[1]);
+  calc_stats(accel_z, MEAS_CNT, &accel_mean[2], &accel_dev[2]);
+  calc_stats(gyro_x, MEAS_CNT, &gyro_mean[0], &gyro_dev[0]);
+  calc_stats(gyro_y, MEAS_CNT, &gyro_mean[1], &gyro_dev[1]);
+  calc_stats(gyro_z, MEAS_CNT, &gyro_mean[2], &gyro_dev[2]);
+
+  // Print debug information out to Serial
+  Serial.println();
+  Serial.print("  ACCEL: ");
+  print_sensor_info(accel_mean, accel_dev, 3);
+  Serial.print("  GYRO: ");
+  print_sensor_info(gyro_mean, gyro_dev, 3);
+
+  // Check that the mean and deviation is within acceptable limits
+  // A deviation of 0 is nearly impossible - there is always some noise expected
+  // Accelerometer
+  for (int i = 0; i < 3; i++) {
+    if (accel_dev[i] == 0 || accel_dev[i] > 1000) {
+      return TestResult(false, "Accelerometer deviation out of bounds");
+    }
+    if (abs(accel_mean[i]) == 16000) {
+      return TestResult(false, "Accelerometer mean indicates failure");
+    }
+  }
+  // Gyrometer
+  for (int i = 0; i < 3; i++) {
+    if (gyro_dev[i] == 0 || gyro_dev[i] > 500) {
+      return TestResult(false, "Gyrometer deviation out of bounds");
+    }
+    if (abs(gyro_mean[i]) >= 500) {
+      return TestResult(false, "Gyrometer mean out of bounds");
+    }
+  }
+  return SUCCESS_RESULT;
+}
+
+TestResult testHMC5883L() {
+  //TODO:
+  return TestResult(false, "Not implemented!");
+}
+
+TestResult testRAM() {
+  // De-init first
+  SPI.end();
+
+  // Initialize SPI and the chip
+  sram.begin();
+
+  // Write different values to different areas in memory
+  // Read them back to verify it works
+  for (uint16_t addr = 20; addr < 60000; addr += 555) {
+    // First do a 0xFF pass
+    if (!SpiRAMTest(addr, 0xFF)) {
+      String msg = "Wiping pass failed: addr=";
+      msg += addr;
+      return TestResult(false, msg);
+    }
+
+    // Then write a value based on the address
+    if (!SpiRAMTest(addr, addr & 0xFF)) {
+      String msg = "Data verification failed: addr=";
+      msg += addr;
+      return TestResult(false, msg);
+    }
+  }
+  return SUCCESS_RESULT;
+}
+
+TestResult testSIM() {
+  // Test whether the SIM908 powered on
+  if (!sim.isOn()) {
+    return TestResult(false, "Failed to turn on SIM");
+  }
+  // Try to send an AT command with several retry attempts
+  for (int i = 0; i < 2 && !sim.writeATCommand("AT"); i++);
+  if (!sim.writeATCommand("AT")) {
+    return TestResult(false, "No response to AT-Command");
+  }
+  return SUCCESS_RESULT;
+}
+
+TestResult testWiFi() {
+  Serial2.begin(115200);
+
+  // Turn on WiFi module
+  enableWiFi();
+
+  // Probe the AT Command and switch up the baud rate until it works
+  boolean found_baud = false;
+  for (int baud_idx = 0; baud_idx < baud_rates_cnt; baud_idx++) {
+    Serial2.begin(baud_rates[baud_idx]);
+    if (writeATCommand(Serial2, "AT\r\n", "OK", 250)) {
+      found_baud = true;
+      break;
+    }
+  }
+  if (!found_baud) {
+    return TestResult(false, "No response to AT-Command");
+  }
+  return SUCCESS_RESULT;
+}
+
+TestResult testBluetooth() {
+  Serial2.begin(9600);
+
+  // Turn on the bluetooth module
+  enableBluetooth();
+
+  // First wake up the device and probe with an AT-command
+  if (!writeATCommand(Serial2, "AT", "OK", 250)) {
+    // In case a connection is currently open, clear it
+    pinMode(BLUETOOTH_KEY_PIN, OUTPUT);
+    digitalWrite(BLUETOOTH_KEY_PIN, LOW);
+    delay(2000);
+    digitalWrite(BLUETOOTH_KEY_PIN, HIGH);
+    
+    // Probe the AT Command and switch up the baud rate until it works
+    boolean found_baud = false;
+    for (int baud_idx = 0; baud_idx < baud_rates_cnt; baud_idx++) {
+      Serial2.begin(baud_rates[baud_idx]);
+      if (writeATCommand(Serial2, "AT", "OK", 500)) {
+        found_baud = true;
+        break;
+      }
+    }
+    if (!found_baud) {
+      return TestResult(false, "No response to AT-Command");
+    }
+  }
+
+  // Reset to factory defaults
+  if (!writeATCommand(Serial2, "AT+RENEW", "OK+RENEW", 200)) {
+    return TestResult(false, "Failed to restore factory defaults");
+  }
+
+  // Reset the device
+  if (!writeATCommand(Serial2, "AT+RESET", "OK+RESET", 200)) {
+    return TestResult(false, "Failed to reset the module");
+  }
+
+  return SUCCESS_RESULT;
+}
+
+TestResult testSD() {
+  // Attempt to initialize the Micro-SD
+  const char name_none[1] = {0};
+  file_open(name_none, name_none, FILE_READ);
+  if (!volume.isInitialized) {
+    return TestResult(false, "Failed to initialize SD card");
+  }
+
+  // Attempt to find the SKETCHES.HEX
+  if (!file_open("SKETCHES", "HEX", FILE_READ)) {
+    return TestResult(false, "Unable to find main sketch");
+  }
+
+  // Make sure to de-initialize the Micro-SD CS pin
+  SD_CS_PORT |= SD_CS_MASK;
+
+  // All good
+  return TestResult(true, "Accessible; No problem found");
+}
+
+TestResult testMIDI() {
+  showMessage("Plug in headphones - hear chimes?\n"
+              "Press SELECT if you hear sound");
+
+  PHN_Midi midi;
+  midi.begin();
+  
+  for (int i = 5; i >= 1; i--) {
+    showCounter(i);
+
+    char notes[] = {40, 41, 42, 43, 44, 45, 35};
+    for (uint8_t i = 0; i < sizeof(notes); i++) {
+      midi.noteOn(0, notes[i], 120);
+      
+      long t = millis();
+      while ((millis() - t) < 150) {
+        if (isSelectPressed()) {
+          midi.noteOff(0, notes[i], 50);
+          return TestResult(true, "User indicated successful");
+        }
+      }
+      midi.noteOff(0, notes[i], 120);
+    }
+  }
+
+  // Failure - no SELECT pressed
+  return TestResult(false, "User indicated no sound");
+}
+
+TestResult testMP3() {
+  showMessage("Plug in headphones - hear beeps?\n"
+              "Press SELECT if you hear sound");
+
+  // Define and initialize the VS1053 audio player here
+  Adafruit_VS1053_FilePlayer musicPlayer(VS1053_RESET_PIN, VS1053_CS_PIN, VS1053_DCS_PIN, VS1053_DREQ_PIN, VS1053_CARDCS_PIN);
+  
+  // Initialize the VS1053 digital pins
+  pinMode(VS1053_GPIO_PIN, OUTPUT);
+  pinMode(VS1053_PWR_PIN, OUTPUT);
+  pinMode(VS1053_DCS_PIN, OUTPUT);
+  digitalWrite(VS1053_PWR_PIN, HIGH);
+  digitalWrite(VS1053_GPIO_PIN, LOW);
+  digitalWrite(VS1053_DCS_PIN, LOW);
+
+  // Initialize the VS1053 library
+  musicPlayer.begin();
+
+  // Make sure to use the pin interrupt
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+
+  // Initialize the SD library for file playback
+  // [!] This has to be done AFTER the initialization of the VS1053 [!]
+  if (!SD.begin(VS1053_CARDCS_PIN)) {
+    return TestResult(false, "Failed to initialize SD card");
+  }
+
+  // Play the test 'beep' file on repeat
+  for (int i = 5; i >= 1; i--) {
+    showCounter(i);
+
+    musicPlayer.startPlayingFile("Sounds/beep.mp3");
+
+    long t = millis();
+    while ((millis() - t) < 900) {
+      if (isSelectPressed()) {
+        return TestResult(true, "User indicated successful");
+      }
+    }
+  }
+
+  // Failure - no SELECT pressed
+  return TestResult(false, "User indicated no sound");
+}
