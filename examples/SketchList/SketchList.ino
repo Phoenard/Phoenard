36248b07d07c7d1e620c572e72b7e59aee50aeae
Irmo van den Berge <irmo.vandenberge@home.nl>
2015-06-19 01:43:34 +0200

    Sketchlist: fix initial invalid state
    Button default was reversed earlier, forgot this bit

--- examples/SketchList/SketchList.ino
+++ examples/SketchList/SketchList.ino
@@ -417,7 +417,7 @@ void editSketch(char filename[9], boolean runWhenExit) {
 
   /* Set up the UI button icons and states */
   const unsigned char* button_icon[EDIT_IDX_NONE] = {
-    edit_icon_changecolor_1, edit_icon_reset,  edit_icon_cancel,
+    edit_icon_changecolor_0, edit_icon_reset,  edit_icon_cancel,
     edit_icon_delete,        edit_icon_charup, edit_icon_accept
   };
   unsigned char button_dirty[EDIT_IDX_NONE + 1];
